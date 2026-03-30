c4aafc3006e79e7bef9b82a4bf19d7aa390544e2
saiswarup <saiswarup825@gmail.com>
2026-03-30 12:41:35 +0530

    deploy

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ProductList
+from .views import ProductList, AddProduct
 
 urlpatterns = [
     path('products/', ProductList.as_view()),
+    path('add-product/', AddProduct.as_view()),
 ]
--- backend/app/views.py
+++ backend/app/views.py
@@ -1,17 +1,25 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.parsers import MultiPartParser, FormParser
 from .models import Product
 from .serializers import ProductSerializer
 
+
 class ProductList(APIView):
+    parser_classes = (MultiPartParser, FormParser)
+
     def get(self, request):
         products = Product.objects.all()
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
+
+
+class AddProduct(APIView):
+    parser_classes = (MultiPartParser, FormParser)
 
     def post(self, request):
         serializer = ProductSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-        return Response(serializer.errors)
+        return Response(serializer.errors, status=400)
